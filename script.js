3ac7e0ae07ed4cadd1bc40912df11c790d8bf505
flourish03 <104957353+flourish03@users.noreply.github.com>
2023-08-02 15:13:13 +0100

    code updated
    this code is meant to arrange pictures like a gallery and there should be change of colors on the text and also background color while clicking on each pictures name

--- script.js
+++ script.js
@@ -12,3 +12,93 @@ change2.addEventListener("click", (event) => {
   event.target.style.color = "red";
 });
 
+const change3 = document.getElementById("image3");
+
+change3.addEventListener("click", (event) => {
+  event.target.style.background = 'yellow';
+  event.target.style.color = "blue";
+});
+
+const change4 = document.getElementById("image4");
+
+change4.addEventListener("click", (event) => {
+  event.target.style.background = 'purple';
+  event.target.style.color = "white";
+});
+
+const change5 = document.getElementById("image5");
+
+change5.addEventListener("click", (event) => {
+  event.target.style.background = 'black';
+  event.target.style.color = "white";
+});
+
+const change6 = document.getElementById("image6");
+
+change6.addEventListener("click", (event) => {
+  event.target.style.background = 'red';
+  event.target.style.color = "white";
+});
+
+const change7 = document.getElementById("image7");
+
+change7.addEventListener("click", (event) => {
+  event.target.style.background = 'pink';
+  event.target.style.color = "yellow";
+});
+
+const change8 = document.getElementById("image8");
+
+change8.addEventListener("click", (event) => {
+  event.target.style.background = 'black';
+  event.target.style.color = "red";
+});
+
+const change9 = document.getElementById("image9");
+
+change9.addEventListener("click", (event) => {
+  event.target.style.background = 'red';
+  event.target.style.color = "yellow";
+});
+
+const change10 = document.getElementById("image10");
+
+change10.addEventListener("click", (event) => {
+  event.target.style.background = 'blue';
+  event.target.style.color = "black";
+});
+
+const change11 = document.getElementById("image11");
+
+change11.addEventListener("click", (event) => {
+  event.target.style.background = 'orange';
+  event.target.style.color = "white";
+});
+
+const change12 = document.getElementById("image12");
+
+change12.addEventListener("click", (event) => {
+  event.target.style.background = 'yellow';
+  event.target.style.color = "black";
+});
+
+const change13 = document.getElementById("image13");
+
+change13.addEventListener("click", (event) => {
+  event.target.style.background = 'red';
+  event.target.style.color = "black";
+});
+
+const change14 = document.getElementById("image14");
+
+change14.addEventListener("click", (event) => {
+  event.target.style.background = 'orange';
+  event.target.style.color = "black";
+});
+
+const change15 = document.getElementById("image15");
+
+change15.addEventListener("click", (event) => {
+  event.target.style.background = 'purple';
+  event.target.style.color = "black";
+});
